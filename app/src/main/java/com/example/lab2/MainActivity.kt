0dc0d0f01c94b63113956fc4cd20c09bb2f1aa0e
Ufgt_NOI <strelok1579@gmail.com>
2025-01-04 19:03:41 +0700

    Добавляем логику программы

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.lab2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val inputField = findViewById<EditText>(R.id.inputField)
+        val checkButton = findViewById<Button>(R.id.checkButton)
+        val resultText = findViewById<TextView>(R.id.resultText)
+
+        checkButton.setOnClickListener {
+            val input = inputField.text.toString()
+
+            val n = input.toInt()
+            var s: Double = 1.00
+            var f: Double = 2.00
+
+            for (i in 2..n){
+                s = s + 1/f
+                f *= (n - i).toDouble()
+            }
+            resultText.text = "$s"
         }
     }
 }
